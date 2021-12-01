7e70eb5009bfaac25591a3d69aec3fbaf97f7082
Maciej Purski <maciej.purski@phoenix-rtos.com>
2021-12-01 15:24:21 +0100

    build-core: Don't build lwip, when CORE_NETWORKING_DISABLE is set
    JIRA: G3-87

--- build-core-armv7a7-imx6ull.sh
+++ build-core-armv7a7-imx6ull.sh
@@ -31,9 +31,11 @@ make -C "phoenix-rtos-usb" all install
 b_log "Building coreutils"
 make -C "phoenix-rtos-utils" all install
 
-b_log "phoenix-rtos-lwip"
-make -C "phoenix-rtos-lwip" all
-b_install "$PREFIX_PROG_STRIPPED/lwip" /sbin
+if [ "$CORE_NETWORKING_DISABLE" != "y" ]; then
+	b_log "phoenix-rtos-lwip"
+	make -C "phoenix-rtos-lwip" all
+	b_install "$PREFIX_PROG_STRIPPED/lwip" /sbin
+fi
 
 b_log "Building posixsrv"
 make -C "phoenix-rtos-posixsrv" all install
