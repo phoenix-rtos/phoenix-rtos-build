71680115a5b476face644e70af4c14c86cba3ff1
Mateusz Bloch <mateusz.bloch@phoenix-rtos.com>
2023-11-09 19:40:45 +0100

    make: adapt Makefile.common for macOS compatibility
    Some options have been modified because they are not available for the Apple linker.
    This change ensures proper functionality on macOS systems.
    
    JIRA: CI-264

--- target/host.mk
+++ target/host.mk
@@ -39,3 +39,12 @@ ROOTFS_INSTALL_UNSTRIPPED := y
 # don't use sysroot on host
 LIBPHOENIX_DEVEL_MODE := n
 PREFIX_SYSROOT :=
+
+# check if linker is an apple linker
+LINKER := $(shell $(CC) -Wl,-v 2>&1)
+ifeq ($(findstring PROJECT:ld, $(LINKER)), PROJECT:ld)
+  LDFLAGS_GC_SECTIONS := $(LDFLAGS_PREFIX)-dead_strip
+  LDFLAGS_WHOLE_ARCHIVE_BEGIN := $(LDFLAGS_PREFIX)-all_load
+  # (set to empty, so ?= won't assign default value)
+  LDFLAGS_WHOLE_ARCHIVE_END := 
+endif
