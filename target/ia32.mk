4e2ee2f63680767c7c994ffbea176b9a2f253482
Hubert Badocha <badocha.hubert@gmail.com>
2023-12-11 13:32:56 +0100

    build: Adapt env to shared libs
    JIRA: RTOS-664

--- target/ia32.mk
+++ target/ia32.mk
@@ -25,6 +25,9 @@ LDFLAGS_PREFIX := -Wl,
 
 LDFLAGS :=
 
+TARGET_PIC_FLAG = -fpic
+TARGET_PIE_FLAG = -fpie
+
 OBJCOPY := $(CROSS)objcopy
 OBJDUMP := $(CROSS)objdump
 
@@ -32,5 +35,9 @@ STRIP := $(CROSS)strip
 
 VADDR_KERNEL_BASE := 0xc0000000
 VADDR_KERNEL_INIT := $(shell printf "0x%x" $$(($(VADDR_KERNEL_BASE) + 0x110000)))
+
+
+LIBPHOENIX_PIC ?= y
+LIBPHOENIX_SHARED ?= y
 
 HAVE_MMU := y
