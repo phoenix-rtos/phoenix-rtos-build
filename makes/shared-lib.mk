b74f360f09c6cfe5254971e0b0ed6df3107a8cc3
Hubert Badocha <badocha.hubert@gmail.com>
2023-12-11 13:32:56 +0100

    Adapt build env to shared libs
    JIRA: RTOS-664

--- makes/shared-lib.mk
+++ makes/shared-lib.mk
@@ -1,4 +1,4 @@
-# Makefile rules for compiling and linking binary file
+# Makefile rules for compiling a shared library
 # supported external variables:
 # - NAME - component/target binary name
 # - LOCAL_SRCS - list of source files relative to current makefile
@@ -6,17 +6,15 @@
 # - LOCAL_HEADERS     - headers to be installed (relative to current makefile)
 # - LOCAL_HEADERS_DIR - headers tree be installed (relative to current makefile) - default "include"
 # - HEADERS           - headers to be installed (relative to project root)
-# - DEP_LIBS - static libraries from current repo needed to be compiled/installed before this component (shortcut for putting something in LIBS and DEPS)
+#
 # - DEPS - list of components from current repo to be completed before starting this one
-# - LIBS - names of the static libs to link the binary against (without .a suffix)
+#
 # - LOCAL_CFLAGS  - additional CFLAGS for current component compilation
-# - LOCAL_CXXFLAGS - additional CXXFLAGS for current component compilation
-# - LOCAL_LDFLAGS - additional LDFLAGS for current component linking
-# - LOCAL_LDLIBS  - additional LDLIBS for current component linking
-# - LOCAL_INSTALL_PATH - custom rootfs dir for the binary to be installed (if not provided - DEFAULT_INSTALL_PATH)
+# - LOCAL_CXXFLAGS  - additional CXXFLAGS for current component compilation
+#
+# - LOCAL_INSTALL_PATH - custom rootfs dir for the shared library to be installed (if not provided - DEFAULT_INSTALL_PATH_SO)
 
-# Global variables (not reset by this script):
-# - ROOTFS_INSTALL_UNSTRIPPED - if non-empty - install binaries into rootfs from PREFIX_PROG (instead of _STRIPPED)
+
 
 
 # directory with current Makefile - relative to the repository root
@@ -26,6 +24,16 @@
 ifeq ($(origin LOCAL_DIR), undefined)
   CLIENT_MAKES := $(filter $(TOPDIR)/%,$(abspath $(MAKEFILE_LIST)))
   LOCAL_DIR := $(patsubst $(TOPDIR)/%,%,$(dir $(lastword $(CLIENT_MAKES))))
+endif
+
+# binary.mk clears all variables it uses so we should expect that they are not set here. Leaving them set would
+# influence next binary.mk call leading to unexpected errors
+ifneq ($(DEP_LIB)$(LIBS)$(LOCAL_LDFLAGS)$(LOCAL_LDLIBS),)
+  $(warning $(NAME): DEP_LIB=$(DEP_LIB))
+  $(warning $(NAME): LIBS=$(LIBS))
+  $(warning $(NAME): LOCAL_LDFLAGS=$(LOCAL_LDFLAGS))
+  $(warning $(NAME): LOCAL_LDLIBS=$(LOCAL_LDLIBS))
+  $(error $(NAME): shared-lib.mk invoked with args reserved for binary.mk)
 endif
 
 # external headers - by default "include" dir - to disable functionality set "LOCAL_HEADERS_DIR := nothing"
@@ -40,26 +48,18 @@ SRCS := $(filter $(LANGUAGE_EXTENSIONS), $(SRCS))
 
 # linking prerequisites
 OBJS.$(NAME) := $(patsubst %,$(PREFIX_O)%.o,$(basename $(SRCS)))
-RESOLVED_LIBS := $(patsubst %,$(PREFIX_A)%.a, $(DEP_LIBS))
-RESOLVED_LIBS += $(patsubst %,$(PREFIX_A)%.a, $(LIBS))
 
 # compilation prerequisites - component order-only dependency
-DEPS += $(DEP_LIBS)
 $(OBJS.$(NAME)): | $(DEPS)
 
-ifneq ($(DYNAMIC_BINARY), y)
-# FIXME: remove once hostutils use binary-dyn.mk
-ifneq ($(TARGET_FAMILY), host)
-LOCAL_LDFLAGS += -static
-endif
-endif
+# Shared lib flags
+SHARED_LIB_LD_FLAGS := $(TARGET_PIC_FLAG) -shared -nostartfiles $(LDFLAGS_PREFIX)--warn-shared-textrel
 
 # potentially custom CFLAGS/CXXFLAGS/LDFLAGS for compilation and linking
-# add ABS_HEADERS_DIR to CFLAGS/CXXFLAGS as a first -I path to build always using local headers instead of installed ones
-$(OBJS.$(NAME)): CFLAGS:=-I"$(ABS_HEADERS_DIR)" $(CFLAGS) $(LOCAL_CFLAGS)
-$(OBJS.$(NAME)): CXXFLAGS:=-I"$(ABS_HEADERS_DIR)" $(CXXFLAGS) $(LOCAL_CXXFLAGS)
-$(PREFIX_PROG)$(NAME): LDFLAGS:=$(LDFLAGS) $(LOCAL_LDFLAGS)
-$(PREFIX_PROG)$(NAME): LDLIBS:=$(LOCAL_LDLIBS) $(LDLIBS)
+# add ABS_HEADERS_DIR to CFLAGS/CXXFLAGS to build always using local headers instead of installed ones
+$(OBJS.$(NAME)): CFLAGS:=-I"$(ABS_HEADERS_DIR)" $(CFLAGS) $(LOCAL_CFLAGS) $(TARGET_PIC_FLAG)
+$(OBJS.$(NAME)): CXXFLAGS:=-I"$(ABS_HEADERS_DIR)" $(CXXFLAGS) $(LOCAL_CXXFLAGS) $(TARGET_PIC_FLAG)
+$(PREFIX_SO)$(NAME).so: LDFLAGS:=$(LDFLAGS) $(SHARED_LIB_LD_FLAGS)
 
 # dynamically generated dependencies (file-to-file dependencies)
 DEPS.$(NAME) := $(patsubst %,$(PREFIX_O)%.d,$(SRCS))
@@ -81,38 +81,29 @@ $(INSTALLED_HEADERS.$(NAME)): $(PREFIX_H)%.h: $(LOCAL_DIR)%.h
 $(INSTALLED_HEADERS_TREE.$(NAME)): $(PREFIX_H)%.h: $(ABS_HEADERS_DIR)/%.h
 	$(HEADER)
 
-# rule for linking binary
-# NOTE: if applied globally, we could remove the $(LINK) variable and put explicit commands here
-# NOTE: disabled $(PHOENIXLIB) direct dependency until it can be removed from $(LIBS)
-$(PREFIX_PROG)$(NAME): $(OBJS.$(NAME)) $(RESOLVED_LIBS) # $(PHOENIXLIB)
+# rule for linking shared lib
+$(PREFIX_SO)$(NAME).so: $(OBJS.$(NAME))
 	$(LINK)
 
 # create component phony targets
-.PHONY: $(NAME) $(NAME)-headers $(NAME)-clean $(NAME)-install
+.PHONY: $(NAME) $(NAME)-headers $(NAME)-clean
 
 $(NAME)-headers: $(INSTALLED_HEADERS.$(NAME)) $(INSTALLED_HEADERS_TREE.$(NAME))
 
-$(NAME): $(NAME)-headers $(PREFIX_PROG_STRIPPED)$(NAME)
+$(NAME): $(NAME)-headers $(PREFIX_SO)$(NAME).so
 
 $(NAME)-clean:
 	@echo "cleaning $(NAME)"
-	@rm -rf $(OBJS.$(NAME)) $(DEPS.$(NAME)) $(PREFIX_PROG)$(NAME) $(PREFIX_PROG_STRIPPED)$(NAME) $(INSTALLED_HEADERS.$(NAME)) $(INSTALLED_HEADERS_TREE.$(NAME))
+	@rm -rf $(OBJS.$(NAME)) $(DEPS.$(NAME)) $(INSTALLED_HEADERS.$(NAME)) $(INSTALLED_HEADERS_TREE.$(NAME)) $(PREFIX_SO)$(NAME).so
 
 # install into the root filesystem
-LOCAL_INSTALL_PATH := $(or $(LOCAL_INSTALL_PATH),$(DEFAULT_INSTALL_PATH))
+LOCAL_INSTALL_PATH := $(or $(LOCAL_INSTALL_PATH),$(DEFAULT_INSTALL_PATH_SO))
 
-# add option to install unstripped binaries
-ifeq ($(ROOTFS_INSTALL_UNSTRIPPED),)
-  ROOTFS_BIN_SRC := $(PREFIX_PROG_STRIPPED)$(NAME)
-else
-  ROOTFS_BIN_SRC := $(PREFIX_PROG)$(NAME)
-endif
-
-$(NAME)-install: $(NAME) $(PREFIX_ROOTFS)$(LOCAL_INSTALL_PATH)/$(NAME)
-$(PREFIX_ROOTFS)$(LOCAL_INSTALL_PATH)/$(NAME): $(ROOTFS_BIN_SRC)
+$(NAME)-install: $(NAME) $(PREFIX_ROOTFS)$(LOCAL_INSTALL_PATH)/$(NAME).so
+$(PREFIX_ROOTFS)$(LOCAL_INSTALL_PATH)/$(NAME).so: $(PREFIX_SO)$(NAME).so
 	$(INSTALL_FS)
 
-# necessary for NAME variable to be correctly set in recepies
+# necessary for NAME variable to be correctly set in recipes
 $(NAME) $(NAME)-clean: NAME:=$(NAME)
 
 ALL_COMPONENTS += $(NAME)
@@ -123,14 +114,10 @@ LOCAL_SRCS :=
 undefine LOCAL_DIR # need to treat LOCAL_DIR="" as a valid (set-extenally) value
 LOCAL_HEADERS :=
 undefine LOCAL_HEADERS_DIR # undefine needed for default value to work in next component
-DEP_LIBS :=
 DEPS :=
 SRCS :=
 HEADERS :=
-LIBS :=
 LOCAL_CFLAGS :=
 LOCAL_CXXFLAGS :=
-LOCAL_LDFLAGS :=
-LOCAL_LDLIBS :=
-LOCAL_INSTALL_PATH :=
-undefine DYNAMIC_BINARY
+undefine LOCAL_INSTALL_PATH
+
