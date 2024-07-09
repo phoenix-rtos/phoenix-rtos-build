6841f6a76fcf4d55899b5617396291bbda50c304
Łukasz Leczkowski <lukasz.leczkowski@phoenix-rtos.com>
2024-07-09 16:41:48 +0200

    wip

--- build-core-ia32-generic.sh
+++ build-core-ia32-generic.sh
@@ -41,3 +41,55 @@ fi
 
 b_log "Building posixsrv"
 make -C "phoenix-rtos-posixsrv" all install
+
+b_log "Building cfs"
+
+# mkdir -p cd cFS/osal/build && \
+# cd cFS/osal/build && \
+# cmake -DOSAL_SYSTEM_BSPTYPE=generic-phoenix -DINSTALL_TARGET_LIST=. -DOSAL_CONFIG_DEBUG_PRINTF=TRUE \
+# 	-DCMAKE_TOOLCHAIN_FILE=../../cfe/cmake/sample_defs/toolchain-phoenix.cmake \
+# 	-DCMAKE_INSTALL_PREFIX="$PREFIX_PROG_STRIPPED/cfs" -DENABLE_UNIT_TESTS=true \
+# 	-DOSAL_CONFIG_DEBUG_PERMISSIVE_MODE=TRUE .. && \
+# 	make && make install && \
+# cd ../../..
+
+# for f in "$PREFIX_PROG_STRIPPED/cfs/"*; do
+# 	if [ -f "$f" ]; then
+# 		b_install "$f" /cfs
+# 	fi
+# done
+
+# mkdir -p "$PREFIX_BUILD/cfs"
+# mkdir -p "$PREFIX_FS/root/cfs"
+
+# b_log "Building cFS (into $PREFIX_BUILD/cfs)"
+
+# set variables for make
+# export SIMULATION=phoenix
+# export O="$PREFIX_BUILD/cfs"
+# export INSTALLPREFIX="/cfs"
+# export DESTDIR="$PREFIX_FS/root"
+
+# (un)set build variables to not interfere with cFS build
+# (build of host tools must use the host compiler)
+env -u CC -u LD -u AS -u AR \
+	EXPORT_CC="$CC" EXPORT_LD="$LD" EXPORT_AS="$AS" EXPORT_AR="$AR" \
+	make -C cFS prep
+
+# export EXPORT_CC="$CC" EXPORT_LD="$LD" EXPORT_AS="$AS" EXPORT_AR="$AR"
+
+# cd "$PREFIX_BUILD/cfs" && \
+# cmake -DOSAL_SYSTEM_BSPTYPE=generic-phoenix -DINSTALL_TARGET_LIST=. -DOSAL_CONFIG_DEBUG_PRINTF=TRUE \
+# 	-DOSAL_CONFIG_INCLUDE_DYNAMIC_LOADER=FALSE -DOSAL_CONFIG_INCLUDE_STATIC_LOADER=FALSE \
+# 	-DCMAKE_TOOLCHAIN_FILE="$PREFIX_PROJECT/cFS/cfe/cmake/sample_defs/toolchain-phoenix.cmake" \
+# 	-DCMAKE_INSTALL_PREFIX="$PREFIX_FS/root/cfs" -DENABLE_UNIT_TESTS=true \
+# 	-DCMAKE_BUILD_TYPE=Debug \
+# 	-DOSAL_CONFIG_DEBUG_PERMISSIVE_MODE=TRUE "$PREFIX_PROJECT/cFS/osal" && \
+# 	make && \
+# 	make install
+
+# this can be run from current env,
+# info is saved in cmake-generated files
+make -C cFS
+make -C cFS install
+
