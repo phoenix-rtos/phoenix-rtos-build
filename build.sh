e5cc758eb537c93f517e03e1da8e0d5da4eeb8b2
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2024-09-27 13:43:55 +0200

    build: build host tools with ASAN disabled
    In phoenixrtos/build there is no libasan.so.8 installed by default which
    results in broken host tools.
    
    JIRA: CI-494

--- build.sh
+++ build.sh
@@ -200,7 +200,7 @@ fi
 if [ "${B_HOST}" = "y" ]; then
 	if [ "$TARGET" != "$_TARGET_FOR_HOST_BUILD" ]; then
 		# if not already building for host - re-exec with clean env
-		(env "$ORIG_ENV" TARGET=$_TARGET_FOR_HOST_BUILD ./phoenix-rtos-build/build.sh host)
+		(env "$ORIG_ENV" NOSAN=1 TARGET=$_TARGET_FOR_HOST_BUILD ./phoenix-rtos-build/build.sh host)
 	else
 		source ./phoenix-rtos-build/build-host-tools.sh
 	fi
