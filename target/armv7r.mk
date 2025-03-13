544afa5232eb8c3a364b725e7cdd1fb69ebafd17
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2025-03-13 13:24:40 +0100

    add armv7r5f-zynqmp target
    JIRA: RTOS-1004

---
+++ target/armv7r.mk
@@ -0,0 +1,48 @@
+#
+# Makefile for Phoenix-RTOS 3
+#
+# ARMv7 Cortex-R options
+#
+# Copyright 2025 Phoenix Systems
+#
+
+CROSS ?= arm-phoenix-
+
+CC := $(CROSS)gcc
+CXX := $(CROSS)g++
+
+# common Cortex-R CFLAGS
+OLVL ?= -O2
+CFLAGS += -mthumb -fomit-frame-pointer -mno-unaligned-access
+
+ifeq ($(TARGET_FAMILY), armv7r5f)
+  CFLAGS += -mcpu=cortex-r5 -mtune=cortex-r5 -mfpu=vfpv3-d16 -mfloat-abi=hard
+endif
+
+VADDR_KERNEL_INIT := $(KERNEL_PHADDR)
+
+LDFLAGS := -Wl,-z,max-page-size=0x10
+
+ifeq ($(KERNEL), 1)
+  CFLAGS += -ffixed-r9
+  LDFLAGS += -Tbss=100000 -Tdata=100000
+  STRIP := $(CROSS)strip
+else
+  CFLAGS += -fpic -fpie -msingle-pic-base -mno-pic-data-is-text-relative
+  # output .rel.* sections to make ELF position-independent
+  LDFLAGS += -Wl,-q
+  STRIP := $(PREFIX_PROJECT)/phoenix-rtos-build/scripts/strip.py $(CROSS)strip --strip-unneeded -R .rel.text
+endif
+
+CXXFLAGS := $(CFLAGS)
+
+AR := $(CROSS)ar
+ARFLAGS := -r
+
+LD := $(CROSS)gcc
+LDFLAGS_PREFIX := -Wl,
+
+OBJCOPY := $(CROSS)objcopy
+OBJDUMP := $(CROSS)objdump
+
+HAVE_MMU := n
