513e873a61e9a5068eb3e876662a70d3633f3cf6
Adam Greloch <adam.greloch@phoenix-rtos.com>
2025-08-14 14:15:23 +0200

    host-tools: build libtinyaes and install rofs headers
    mkrofs from hostutils now depends on libtinyaes and uses unified rofs
    layout definition
    
    JIRA: NIL-693

--- build-host-tools.sh
+++ build-host-tools.sh
@@ -4,7 +4,11 @@
 # Builder for Phoenix-RTOS host utils
 
 b_log "Building host corelibs"
-make -C "phoenix-rtos-corelibs" libptable
+make -C "phoenix-rtos-corelibs" libptable libtinyaes
+
+# librofs-headers needed for mkrofs from p-r-hostutils
+b_log "Building filesystem headers"
+make -C "phoenix-rtos-filesystems" librofs-headers
 
 b_log "Building hostutils"
 make -C "phoenix-rtos-hostutils" all
