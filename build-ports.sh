d202e1e9d07cc6cb65a9d41de3b0f6b89fa4ac81
Michal Lach <michal.lach@phoenix-rtos.com>
2026-05-25 15:40:08 +0200

    port_manager: use future minor version as PHOENIX_VER
    On development branches PHOENIX_VER should refer to next tracking
    version, to allow for creation of new ports that are unsupported on
    previous releases.
    
    JIRA: RTOS-1088

--- build-ports.sh
+++ build-ports.sh
@@ -12,17 +12,14 @@ PORT_MANAGER_FLAGS=(
 )
 
 function port_manager() {
-  cd "${PREFIX_PROJECT}/phoenix-rtos-build/" || exit
-  python3 -m "port_manager.main" "${PORT_MANAGER_FLAGS[@]}" "${@}"
+	cd "${PREFIX_PROJECT}/phoenix-rtos-build/" || exit
+	python3 -m "port_manager.main" "${PORT_MANAGER_FLAGS[@]}" "${@}"
 }
 
 if [ "$RAW_LOG" != 1 ]; then
-  PORT_MANAGER_FLAGS+=("-r")
+	PORT_MANAGER_FLAGS+=("-r")
 fi
-
-DUMMY_VERSION="v3.3.1-0-g"
-GIT_DESC="$(cd "./phoenix-rtos-build" && git describe --tags --abbrev=0 --match "v[[:digit:]].[[:digit:]]*.[[:digit:]]*" 2> /dev/null || echo "${DUMMY_VERSION}")"
 
 b_log "Installing ports"
 
-PHOENIX_VER="${GIT_DESC}" port_manager build "${PORTS_CONFIG}" "${PREFIX_PROJECT}/phoenix-rtos-ports"
+PHOENIX_VER="v3.4.0" port_manager build "${PORTS_CONFIG}" "${PREFIX_PROJECT}/phoenix-rtos-ports"
